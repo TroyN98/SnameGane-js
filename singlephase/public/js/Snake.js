0a83f11691988f463d04a0d08e33d50a64964d04
Thabo Troy Ndlovu <thabotroyn98@gmail.com>
2026-01-07 21:18:13 +0200

    stop player from turning back on themselves

--- singlephase/public/js/Snake.js
+++ singlephase/public/js/Snake.js
@@ -2,7 +2,7 @@ export default class Snake {
     constructor(scene) {
         this.scene = scene;
         this.lastMoveTime = 0;
-        this.moveInterval = 200; //millisecs
+        this.moveInterval = 100; //millisecs
         this.tileSize = 16;
         this.body = [];
         this.direction = Phaser.Math.Vector2.DOWN;
@@ -29,16 +29,20 @@ export default class Snake {
 
         switch (event.keyCode) {
             case 37: //left
-                this.direction = Phaser.Math.Vector2.LEFT;
+                if (this.direction !== Phaser.Math.Vector2.RIGHT)
+                    this.direction = Phaser.Math.Vector2.LEFT;
                 break;
             case 38: //up
-                this.direction = Phaser.Math.Vector2.UP;
+                if (this.direction !== Phaser.Math.Vector2.DOWN)
+                    this.direction = Phaser.Math.Vector2.UP;
                 break;
             case 39: //right
-                this.direction = Phaser.Math.Vector2.RIGHT;
+                if (this.direction !== Phaser.Math.Vector2.LEFT)
+                    this.direction = Phaser.Math.Vector2.RIGHT;
                 break;
             case 40: //down
-                this.direction = Phaser.Math.Vector2.DOWN;
+                if (this.direction !== Phaser.Math.Vector2.UP)
+                    this.direction = Phaser.Math.Vector2.DOWN;
                 break;
         }
     }
@@ -74,7 +78,7 @@ export default class Snake {
         if (this.body[0].x < 0 ||
             this.body[0].x >= this.scene.game.config.width ||
             this.body[0].y < 0 ||
-            this.body[0].y >= this.scene.game.config.heighssst) {
+            this.body[0].y >= this.scene.game.config.height) {
 
             this.scene.scene.restart();
         }
